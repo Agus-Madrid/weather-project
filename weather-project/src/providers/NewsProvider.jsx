3cee702d377ef77152e716788a9616f4133888da
rodriaguillon <rodrigo.aguillon@fing.edu.uy>
2024-07-30 14:05:15 -0300

    added some keywords to the NewsProvider

--- weather-project/src/providers/NewsProvider.jsx
+++ weather-project/src/providers/NewsProvider.jsx
@@ -14,20 +14,23 @@ export const NewsProvider = ({ children }) => {
             const formattedDate = fromDate.toISOString().split('T')[0];
 
             let query = 'calm weather';
+            let relevantKeywords = '';
+
             if (weather) {
                 if (weather.wind.speed >= 11) {
                     query = 'windy';
+                    relevantKeywords = 'surf, kites';
                 } else if (weather.main.temp > 20) {
                     query = 'heat';
+                    relevantKeywords = 'beach, UV, solar protector';
                 } else if (weather.clouds && weather.clouds.all > 50) {
                     query = 'cloudy';
+                    relevantKeywords = 'rain, rainy';
                 }
             }
-            // console.log('Query:', query);
 
-            const response = await fetch(`https://newsapi.org/v2/everything?q=${query}&from=${formattedDate}&sortBy=publishedAt&apiKey=d8e96e18a2bb45b881d33a8f793151df`);
+            const response = await fetch(`https://newsapi.org/v2/everything?q=${query} ${relevantKeywords}&from=${formattedDate}&sortBy=publishedAt&apiKey=d8e96e18a2bb45b881d33a8f793151df`);
             const data = await response.json();
-            // console.log('News data:', data);
             setNews(data.articles);
         } catch (error) {
             console.error('Error fetching news:', error);
